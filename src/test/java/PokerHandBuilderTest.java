eb913bb77f94f3abd7ac374b011e5178e5a600d2
Camilo Sanchez <camilin87@gmail.com>
2018-02-22 06:13:57 -0500

    minor tests rename

--- src/test/java/PokerHandBuilderTest.java
+++ src/test/java/PokerHandBuilderTest.java
@@ -1,8 +1,15 @@
 import org.junit.Test;
 
 public class PokerHandBuilderTest {
+    private final PokerHandBuilder builder = new PokerHandBuilder();
+
     @Test(expected = IllegalArgumentException.class)
-    public void failsWhenInputIsEmpty(){
-        new PokerHandBuilder().build("");
+    public void failsWhenNull(){
+        builder.build(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsWhenEmpty(){
+        builder.build("");
     }
 }
