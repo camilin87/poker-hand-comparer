9e2da0082cbf577bbe674e58858591c41058875e
Camilo Sanchez <camilin87@gmail.com>
2018-02-22 08:00:07 -0500

    refactor the Hand parse method to conform with the new better standards

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -1,26 +1,40 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Hand {
-    public String[] parse(String encodedHand){
+    private final Card[] cards;
+
+    private Hand(Card[] cards){
+        if (cards.length != 5){
+            throw new IllegalArgumentException();
+        }
+
+        this.cards = cards;
+    }
+
+    @Override
+    public String toString() {
+        List<String> result = Arrays.stream(cards)
+                .map(Card::toString)
+                .collect(Collectors.toList());
+        return String.join(" ", result);
+    }
+
+    public static Hand parse(String encodedHand){
         if (encodedHand == null || encodedHand == ""){
             throw new IllegalArgumentException();
         }
 
         String[] cards = encodedHand.split(" ");
-        if (cards.length != 5){
-            throw new IllegalArgumentException();
-        }
-
         if (Arrays.stream(cards).anyMatch(c -> c.length() != 2)){
             throw new IllegalArgumentException();
         }
 
-        return Arrays.stream(cards)
+        Card[] handCards = Arrays.stream(cards)
                 .map(Card::parse)
-                .map(Card::toString)
                 .collect(Collectors.toList())
-                .toArray(new String[0]);
-
+                .toArray(new Card[0]);
+        return new Hand(handCards);
     }
 }
--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -1,47 +1,44 @@
 import org.junit.Test;
-import java.util.Arrays;
+import static org.junit.Assert.*;
 
 public class HandTest {
-    private final Hand builder = new Hand();
-
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenNull(){
-        builder.parse(null);
+        Hand.parse(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenEmpty(){
-        builder.parse("");
+        Hand.parse("");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenLessThanFiveCards(){
-        builder.parse("2D 3D 4D 5D");
+        Hand.parse("2D 3D 4D 5D");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenMoreThanFiveCards(){
-        builder.parse("3D 3D 4D 5D 6D 7D");
+        Hand.parse("3D 3D 4D 5D 6D 7D");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenCardTooShort(){
-        builder.parse("D 2D 3D 4D 5D");
+        Hand.parse("D 2D 3D 4D 5D");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenCardTooLong(){
-        builder.parse("10D 2D 3D 4D 5D");
+        Hand.parse("10D 2D 3D 4D 5D");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsWhenInvalidClub(){
-        builder.parse("2C 3D 4H 5S 6I");
+        Hand.parse("2C 3D 4H 5S 6I");
     }
 
     @Test
     public void buildsHand(){
-        String[] expected = {"2D", "3D", "4D", "5D", "6D"};
-        assert Arrays.equals(expected, builder.parse("2D 3D 4D 5D 6D"));
+        assertEquals("2D 3D 4D 5D 6D", Hand.parse("2D 3D 4D 5D 6D").toString());
     }
 }
