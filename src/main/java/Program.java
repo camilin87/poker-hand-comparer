caf21f007248cecf943dd5733262563596c99a96
Camilo Sanchez <camilin87@gmail.com>
2018-02-24 09:45:56 -0500

    parse the hand arguments

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Program {
     public static void main(String[] args){
-        System.out.print("Proker Hand Evaluator");
+        System.out.println("Poker Hand Evaluator");
+
+        System.out.println("Parsing:");
+        Arrays.stream(args).forEach(System.out::println);
+
+        String result = Arrays.stream(Hand.parseMultiple(args))
+                .sorted()
+                .skip(Math.max(args.length - 1, 0))
+                .limit(1)
+                .map(Hand::toString)
+                .findFirst()
+                .orElse("INVALID ARGS");
+
+        System.out.println("--");
+        System.out.println(result);
     }
 }
